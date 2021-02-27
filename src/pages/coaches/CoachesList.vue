e8ec60ebfa4fe1d497d6c398fbeefee661183789
N1Anoobis <mrbond84@hotmail.com>
2021-02-27 15:28:13 +0100

    Add basic vuex stores structure

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,9 +6,26 @@
         <button>Refresh</button>
         <router-link to="/register">Register</router-link>
       </div>
-      <ul>
-        list of coaches
+      <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+          {{ coach.firstName }}
+        </li>
       </ul>
+      <h3 v-else>No coaches found.</h3>
     </section>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class CoachesList extends Vue {
+  get filteredCoaches() {
+    return this.$store.getters["coaches/coaches"];
+  }
+
+  get hasCoaches() {
+    return this.$store.getters["coaches/hasCoaches"];
+  }
+}
+</script>
